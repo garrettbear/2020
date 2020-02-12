0df344210b9e6c34a436809f073e9db424152dbd
Garrett Bear <gwhisten@gmail.com>
2020-02-11 23:10:57 -0800

    colorful sticky projects

--- src/App.js
+++ src/App.js
@@ -3,12 +3,45 @@ import React from "react";
 function App() {
   return (
     <div className="app">
-      <section>
-        <div className="container">
-          <div className="overlay">
-            <div className="overlay--inner">This is the inner</div>
+      <header>
+        <div className="">
+          <h1>GarrettBear</h1>
+          <h5>Product Design Engineer</h5>
+        </div>
+      </header>
+      <section className="flex">
+        <div className="item stuck align-right">
+          <h1>Ad Manager 2.0</h1>
+          <h3>GumGum</h3>
+          <p>UX/UI</p>
+        </div>
+        <div className="item long">
+          <div className="project-info">
+            <ol>
+              <li>this is an item</li>
+              <li>this is an item</li>
+              <li>this is an item</li>
+              <li>this is an item</li>
+              <li>this is an item</li>
+              <li>this is an item</li>
+              <li>this is an item</li>
+            </ol>
           </div>
         </div>
+      </section>
+
+      <section className="flex">
+        <div className="item long">Right</div>
+        <div className="item stuck">
+          <h1>Ad Manager 2.0</h1>
+          <h3>GumGum</h3>
+          <p>UX/UI</p>
+        </div>
+      </section>
+
+      <section className="flex">
+        <div className="item stuck">Left</div>
+        <div className="item long">Right</div>
       </section>
 
       <section>
@@ -26,6 +59,10 @@ function App() {
           </div>
         </div>
       </section>
+
+      <footer>
+        <h1>GarrettBear</h1>
+      </footer>
     </div>
   );
 }
