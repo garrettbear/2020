9ba3f3e827687d524286b0d299796f22406aa75f
Garrett Bear <gwhisten@gmail.com>
2020-02-11 22:22:33 -0800

    made it sticky

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from "react";
 
 function App() {
   return (
-    <div className="layout">
-      <h1>This is the example</h1>
-      <h2>This is the example</h2>
-      <h3>This is the example</h3>
-      <h4>This is the example</h4>
-      <h5>This is the example</h5>
-      <p>
-        Okay so this is a paragraphe with{" "}
-        <a href="/"> a link garrett inside it</a> and a{" "}
-        <span> random span right here</span>
-      </p>
-      <div>normal div with some text for no reason</div>
-      <div>
-        <b>using the bold element</b>
-      </div>
-      <ol>
-        <li>list item</li>
-        <li>another list item</li>
-      </ol>
+    <div className="app">
+      <section>
+        <div className="container">
+          <div className="overlay">
+            <div className="overlay--inner">This is the inner</div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="overlay">
+            <div className="overlay--inner">This is the inner</div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="overlay">
+            <div className="overlay--inner">This is the inner</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
