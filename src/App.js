778caba9de1560b38e848ce2481d91477299ca5d
Garrett Bear <gwhisten@gmail.com>
2020-02-11 21:28:50 -0800

    some new styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { useRef, useLayoutEffect } from "react";
-import "./App.css";
-
-const isBrowser = typeof window !== `undefined`;
+import React from "react";
 
 function App() {
   return (
     <div className="layout">
       <h1>This is the example</h1>
+      <h2>This is the example</h2>
+      <h3>This is the example</h3>
+      <h4>This is the example</h4>
+      <h5>This is the example</h5>
+      <p>
+        Okay so this is a paragraphe with{" "}
+        <a href="/"> a link garrett inside it</a> and a{" "}
+        <span> random span right here</span>
+      </p>
+      <div>normal div with some text for no reason</div>
+      <div>
+        <b>using the bold element</b>
+      </div>
+      <ol>
+        <li>list item</li>
+        <li>another list item</li>
+      </ol>
     </div>
   );
 }
