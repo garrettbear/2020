abcb5f260650c3cb64b8e6cd69c5b3e97dcfbf51
Garrett Bear <gwhisten@gmail.com>
2021-06-23 23:35:10 -0700

    stage created

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// Initial Load Receipt is over 100vh
+// On scroll when towards bottom of receipt wrapper to load next item
+// once all items have loaded to begin loading random items
+
+export default function Home() {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
